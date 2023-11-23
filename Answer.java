c69360a0d0579c1a80feb1c3eb2105a6935c1006
eunsu14 <7266706@naver.com>
2023-11-23 10:15:21 +0900

    Update Answer.java

--- Answer.java
+++ Answer.java
@@ -1,60 +1,78 @@
-// Q&A 게시판 답변 글 관리
-public class Answer {
-	private int cId; // 답변 글 번호
-	private Question board; // Q&A 게시판 질문 글(QNA_Board) 참조
-	private String nickname; // 작성자(닉네임 사용)
-	private String date; // 글 작성한 시간
-	private String answerComment; // 답변 글 내용
-	
-	public Answer() { }
-	
-	public Answer(int cId, Question board, String nickname,
-			String date, String answerComment) {
-		this.cId = cId;
-		this.board = board;
-		this.nickname = nickname;
-		this.date = date;
-		this.answerComment = answerComment;
-	}
-	
-	public int getCId() {
-		return cId;
-	}
-	
-	public void setCId(int cId) {
-		this.cId = cId; 
-	}
-	
-	public Question getBoard() {
-		return board;
-	}
-	
-	public void setBoard(Question board) {
-		this.board = board;
-	}
-	
-	public String getNickname() {
-		return nickname;
-	}
-	
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-	
-	public String getDate() {
-		return date;
-	}
-	
-	public void setDate(String date) {
-		this.date = date;
-	}
-	
-	public String getAnswerComment() {
-		return answerComment;
-	}
-	
-	public void setAnswerComment(String answerComment) {
-		this.answerComment = answerComment;
-	}
+package service.dto;
 
+import java.util.Date;
+
+public class Answer {
+    private int aId;
+    private String answer;
+    private int qId;
+    private String userId;
+    private String nickname;
+    private Date createDate;
+    
+    public Answer() {}
+
+    public Answer(int aId, String answer, int qId, String userId, Date createDate) {
+        super();
+        this.aId = aId;
+        this.answer = answer;
+        this.qId = qId;
+        this.userId = userId;
+        this.createDate = createDate;
+    }
+
+    public Answer(int aId, String answer, String nickname, int qId, Date createDate) {
+        super();
+        this.aId = aId;
+        this.answer = answer;
+        this.nickname = nickname;
+        this.qId = qId;
+        this.createDate = createDate;
+    }
+
+    public int getaId() {
+        return aId;
+    }
+
+    public void setaId(int aId) {
+        this.aId = aId;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public int getqId() {
+        return qId;
+    }
+
+    public void setqId(int qId) {
+        this.qId = qId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer [aId=" + aId + ", answer=" + answer + ", qId=" + qId + ", userId=" + userId + ", createDate="
+                + createDate + "]";
+    }
 }
